11759fa98a689058f52398d0235dc7bcdf1e8742
bluewhaleio <bluewhale>
2017-08-19 22:33:21 +0000

    new code

---
+++ src/main/java/com/fmr/AP1234/Utils.java
@@ -0,0 +1,12 @@
+package com.fmr.AP1234;
+
+public class Utils {
+    private String name;
+
+    public Utils(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/fmr/App.java
@@ -0,0 +1,18 @@
+package com.fmr;
+
+import com.fmr.AP1234.Utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Utils u = new Utils("sridhar");
+
+        System.out.println(u.getName());
+    }
+}
